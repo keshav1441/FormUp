5eef0a93dea5593ddcaaeca526a96f586506aaeb
keshav1441 <keshavsharma1441@gmail.com>
2024-12-16 17:43:53 +0530

    Preview Working

--- components/fields/TextField.tsx
+++ components/fields/TextField.tsx
@@ -51,7 +51,7 @@ export const TextFieldFormElement: FormElement = {
     label: "Text Field",
   },
   designerComponent: DesignerComponent, 
-  formComponent: () => <div>Form component</div>, 
+  formComponent: FormComponent, 
   propertiesComponent: PropertiesComponent, 
 };
 
@@ -214,6 +214,31 @@ function DesignerComponent({
       <Input
         readOnly
         disabled
+        placeholder={placeHolder}
+      />
+      {helperText && (
+        <p className="text-muted-foreground text-[0.8rem]">{helperText}</p>
+      )}
+    </div>
+  );
+}
+
+
+function FormComponent({
+  elementInstance,
+}: {
+  elementInstance: FormElementInstance;
+}) {
+  const element = elementInstance as CustomInstance;
+  const { label, required, placeHolder, helperText } = element.extraAttributes;
+
+  return (
+    <div className="flex flex-col gap-2 w-full">
+      <Label>
+        {label}
+        {required && "*"}
+      </Label>
+      <Input
         placeholder={placeHolder}
       />
       {helperText && (
