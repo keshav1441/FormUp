196f9b0622bb9e4a30e23ed02a8185371b4ebb0e
keshav1441 <keshavsharma1441@gmail.com>
2024-12-24 02:50:37 +0530

    erroes

--- app/(dashboard)/builder/[id]/page.tsx
+++ app/(dashboard)/builder/[id]/page.tsx
@@ -2,17 +2,15 @@ import React from "react";
 import FormBuilder from "@/components/FormBuilder"; // Adjust the import according to your structure
 import { GetFormById } from "@/action/form";
 
-
 async function BuilderPage({ params }: { params: { id: string } }) {
-    const {id} = await params
-    const form = await GetFormById(Number(id)); 
-    if (!form){
-        throw new Error("Form not found")
-    }
-
-  return (
-    <FormBuilder form={form}/>
-  );
+  const { id } = params; // No need to await here
+  const form = await GetFormById(Number(id));
+  
+  if (!form) {
+    throw new Error("Form not found");
+  }
+  
+  return <FormBuilder form={form} />;
 }
 
 export default BuilderPage;
